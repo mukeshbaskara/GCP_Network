58b40915dc21e00d810d47712b15b384583f820b
chitraabanues <chitraabanues@gmail.com>
2023-01-01 21:07:25 +0530

    fixed the param

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "vpc" {
-  source                          = "./modules/vpc"
-  network_name                    = var.network_name
-  auto_create_subnetworks         = var.auto_create_subnets
-  routing_mode                    = var.routing_mode
-  project_id                      = var.project_id
-  description                     = var.description
-  delete_default_routes_on_create = var.delete_default_internet_gateway_routes
-  mtu                             = var.mtu
+  source                                  = "./modules/vpc"
+  network_name                            = var.network_name
+  auto_create_subnetworks                 = var.auto_create_subnets
+  routing_mode                            = var.routing_mode
+  project_id                              = var.project_id
+  description                             = var.description
+  delete_default_internet_gateway_routes  = var.delete_default_internet_gateway_routes
+  mtu                                     = var.mtu
 }
 
 module "subnet" {
